a40eded0d3da820bbd30f93ab9b7dfd6fb4d8622
NishanNiriella <it19026480@my.sliit.lk>
2022-03-29 16:23:57 +0530

    Add_event_helpcard_added_20220329

--- lib/screens/event/add_event.dart
+++ lib/screens/event/add_event.dart
@@ -1,6 +1,7 @@
 import 'package:family_tree/Model/member.dart';
 import 'package:family_tree/components/event/add_event_form.dart';
 import 'package:flutter/material.dart';
+import 'package:giff_dialog/giff_dialog.dart';
 
 class AddEventScreen extends StatelessWidget {
   final List<Member> allParticipants;
@@ -36,6 +37,50 @@ class AddEventScreen extends StatelessWidget {
             'Add Event',
             style: TextStyle(color: Colors.white),
           ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (_) => AssetGiffDialog(
+                          onlyOkButton: true,
+                          onOkButtonPressed: () {
+                            Navigator.pop(context);
+                          },
+                          buttonOkText: const Text("Got it!",
+                              style: TextStyle(
+                                color: Colors.white,
+                              )),
+                          buttonOkColor: Colors.blueAccent,
+                          image: Image.asset(
+                            'assets/generation/genZ.gif',
+                            fit: BoxFit.fill,
+                          ),
+                          title: const Text(
+                            "Adding an Upcoming Event",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          description: const Text(
+                            "Event Name, Date, Time, Participants and Location must be there in order to add an upcoming event",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ));
+              },
+              icon: const Padding(
+                padding: EdgeInsets.only(right: double.infinity),
+                child: Icon(
+                  Icons.help,
+                  size: 30.0,
+                ),
+              ),
+            )
+          ],
         ),
         body: SafeArea(
           child: Column(
